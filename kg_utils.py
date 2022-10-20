efbf7bcf810f385039677570f55f982d64cb6315
linnil1 <linnil1.886@gmail.com>
2022-10-20 21:03:40 +0800

    Add slurm executor

--- kg_utils.py
+++ kg_utils.py
@@ -1,19 +1,21 @@
 import os
-from glob import glob
+import sys
+import time
 import uuid
+import pickle
+import __main__
 import subprocess
-from concurrent.futures import ProcessPoolExecutor
+from glob import glob
+from typing import Callable
+from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor
 
+from namepipe import BaseTaskExecutor, NamePath
 from graphkir.utils import (
     runShell,
-    samtobam,
     runDocker as runDockerGK,
-    getGeneName,
-    limitAlleleField,
-    getAlleleField,
 )
 
-threads = 20
 images = {
     'samtools': "quay.io/biocontainers/samtools:1.15.1--h1170115_0",
     'clustalo': "quay.io/biocontainers/clustalo:1.2.4--h1b792b2_4",
@@ -35,3 +37,99 @@ def runDocker(image: str, cmd: str, *arg, **kwargs):
     """ run docker container """
     image = images.get(image, image)
     return runDockerGK(image, cmd, *arg, **kwargs)
+
+
+def runSingularity(
+    image: str, cmd: str, capture_output=False, cwd=None
+) -> subprocess.CompletedProcess:
+    """ run siguarity which image is downloaded as file """
+    image = images.get(image, image)
+    return runShell(
+        "singularity run "
+        "--bind /staging/biology "
+        "--bind /staging/biodata/lions/twbk/ "
+        "--bind /work/linnil1tw/ "
+        f"{image.split('/')[-1]}.sif "
+        f"{cmd}",
+        capture_output=capture_output,
+    )
+
+
+class IsolateTaskExecutor(BaseTaskExecutor):
+    """ Run the task in different process """
+
+    def __init__(self, threads: int = 100):
+        super().__init__()
+        self.executor = f"python isolate_task.py {__main__.__file__}"
+        self.threads = threads
+
+    def startIsolateTask(self, name: str) -> subprocess.CompletedProcess:
+        """ Define how to run the task in isolated python """
+        return runShell(f"{self.executor} {name}")
+
+    def submit(self, func: Callable, input_name: NamePath) -> str:
+        """
+        Setup the data needed for isolate task.
+        * name
+        * {name}.tmp.in
+        * {name}.tmp.out
+        """
+        name = "job_" + str(input_name).replace("/", "_")
+        # create tmp input parameters
+        pickle.dump(
+            (func, input_name), open(f"{name}.tmp.in", "wb"), pickle.HIGHEST_PROTOCOL
+        )
+        # submit/start each task
+        self.startIsolateTask(name)
+        return f"{name}.tmp.out"
+
+    def run_tasks(self, names: list[NamePath], func: Callable) -> list[NamePath | str]:
+        """
+        Run the isolated tasks in threads pool to avoid blocking.
+
+        If task preparion or task submittion fail, it'll immediatly fail.
+        Otherwise, it will raise error after all processes are done.
+        """
+        # for name in names:
+        #     output_tmp_files.append(self.submit(func, name))
+        with ThreadPoolExecutor(max_workers=self.threads) as executor:
+            exes = [executor.submit(self.submit, func, name) for name in names]
+            output_tmp_files = [exe.result() for exe in exes]
+
+        # use xx.tmp.out to check if the task is done
+        output_name = []
+        for output_tmp_file in output_tmp_files:
+            while not os.path.exists(output_tmp_file):
+                time.sleep(3)
+            output_name.append(pickle.load(open(output_tmp_file, "rb")))
+
+        # print and raise error if return Error
+        has_error = False
+        for i in output_name:
+            if isinstance(output_name[-1], BaseException):
+                print("ERROR", output_name[-1])
+                has_error = True
+        if has_error:
+            raise ValueError("Some tasks have error")
+        return output_name
+
+
+class SlurmTaskExecutor(IsolateTaskExecutor):
+    """ Run the task in SLURM """
+
+    def __init__(self, template_file="taiwania.template"):
+        super().__init__(threads=1)
+        self.template = open(template_file).read()
+
+    def startIsolateTask(self, name: str) -> subprocess.CompletedProcess:
+        """
+        Save the command into shell script(xx.tmp.sh).
+
+        And submit it to SLURM.
+
+        The return process is the process to submit job (<1s),
+        so, it will not block when task is not finished.
+        """
+        text = self.template.format(cmd=f"{self.executor} {name}", name=name)
+        open(f"{name}.tmp.sh", "w").write(text)
+        return runShell(f"sbatch {name}.tmp.sh")
